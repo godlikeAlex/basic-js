5261ab8bfda8820b0003c3df5126ef691cb773ed
godlikeAlex <godlikedesigner@gmail.com>
2025-01-06 17:41:57 +0500

    feat: resovle get matrix elements sum

--- src/matrix-elements-sum.js
+++ src/matrix-elements-sum.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given matrix, a rectangular matrix of integers,
@@ -16,11 +16,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be 9
  */
-function getMatrixElementsSum(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getMatrixElementsSum(matrix) {
+  const columnCount = matrix[0].length;
+  let sum = 0;
+
+  for (let i = 0; i < columnCount; i++) {
+    for (let j = 0; j < matrix.length; j++) {
+      const columnValue = matrix[j][i];
+
+      if (columnValue == 0) {
+        break;
+      }
+
+      sum += columnValue;
+    }
+  }
+
+  return sum;
 }
 
 module.exports = {
-  getMatrixElementsSum
+  getMatrixElementsSum,
 };
